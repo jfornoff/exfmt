26ff60dcf7c333ec866cb08c6ed9e52d9ef4b417
Jan Fornoff <janfornoff@gmail.com>
2017-07-31 19:50:01 +0200

    Added test for long and short pipelines in anonymous functions

--- test/exfmt/integration/pipeline_test.exs
+++ test/exfmt/integration/pipeline_test.exs
@@ -2,16 +2,28 @@ defmodule Exfmt.Integration.PipelineTest do
   use ExUnit.Case
   import Support.Integration
 
-  test "pipeline in anonymous functions" do
-    """
-    Task.start(fn -> rp |> capture_samplers |> do_some_business |> StoreReportingPeriod.report! end)
-    """ ~> """
-    Task.start fn ->
-                 rp
-                 |> capture_samplers
-                 |> do_some_business
-                 |> StoreReportingPeriod.report!
-               end
-    """
+  describe "pipeline in anonymous functions" do
+    test "short pipeline" do
+      # Short, non-wrapping example
+      """
+      Task.start(fn -> apple |> eat end)
+      """ ~> """
+      Task.start fn -> apple |> eat end
+      """
+    end
+
+    test "long pipeline" do
+      # Long, wrapping example
+      """
+      Task.start(fn -> banana |> peel_banana |> slice_banana |> eat_banana end)
+      """ ~> """
+      Task.start fn ->
+                   banana
+                   |> peel_banana
+                   |> slice_banana
+                   |> eat_banana
+                 end
+      """
+    end
   end
 end
